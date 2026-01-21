1dc6ef379ac006809e1a90c0763c47d3272d0e8c
nicolapastorino <nicolapastorino@hotmail.com>
2026-01-21 23:46:26 +0100

    Add resource group and virtual network

---
+++ 01-base-infra/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-base"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
